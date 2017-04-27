f2f19888d9659271e23f5bebc7237b071525c2e3
The Octocat <yezero@users.noreply.github.com>
2017-04-28 09:59:24 +1000

    Update authKeys.js

--- back/app/Omniauth/authKeys.js
+++ back/app/Omniauth/authKeys.js
@@ -5,7 +5,7 @@
 module.exports = {
 
     'facebook' : {
-        input_token  : 'XXX',
+        input_token  : '4f23c40ecec3cf36b887a764ccd31faf',
         fields : 'last_name,first_name,picture'
     },
     'ecole42' : {
@@ -17,17 +17,17 @@ module.exports = {
     'github' : {
         client_id: 'bbb90e851a575c721804',
         client_secret: 'e6beb1592e6113d8efa2c593ebd3d1041849445c',
-        redirect_uri: 'http://107.170.35.172'
+        redirect_uri: 'http://app.tunepods.com'
     },
     'slack' : {
         client_id: 'XXX',
-        client_secret: 'XXX'
+        client_secret: 'http://app.tunepods.com'
     },
     'linkedin' : {
         client_id: "XXX",
         client_secret: 'XXX',
         grant_type: 'authorization_code',
-        redirect_uri: 'http://107.170.35.172/?from=Linkedin'
+        redirect_uri: 'http://app.tunepods.com/?from=Linkedin'
     }
 };
 
